d96b7c6881908ceb494619e4e66f873092cbaf53
Wil Son <lohwilson@hotmail.com>
2020-08-29 16:42:25 +0800

    added js file

---
+++ js/todo.js
@@ -0,0 +1,15 @@
+//way to store todos
+let todos = ['item1', 'item2', 'item3'];
+
+//way to display todos
+console.log('My Todos:' + todos);
+
+//way to add todos
+todos.push('item4');
+
+//way to change todos
+todos[0] = 'item001';
+
+//way to delete todos
+todos.splice(0,1);  //.splice removes (index of todo, no of items)
+
